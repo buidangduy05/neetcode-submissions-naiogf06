5a7a7e2abeaf3f479a07c6013be0708881d94895
buidangduy05 <buidangduy243@gmail.com>
2026-04-21 20:31:48 +0700

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        string res = "";  
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != ']') res.push_back(s[i]);
+            else {
+                string temp = "";
+                while (!res.empty() && res.back() != '[') {
+                    temp.push_back(res.back());
+                    res.pop_back();
+                }
+                reverse(temp.begin(), temp.end());
+                res.pop_back();
+                string num = "";
+                while (!res.empty() && res.back() >= '0' && res.back() <= '9') {
+                    num.push_back(res.back());
+                    res.pop_back();
+                }
+                reverse(num.begin(), num.end());
+                int p = stoi(num);
+                while (p--) {
+                    res.append(temp);
+                }
+            }
+        }
+        return res;
+    }       
+};
