e0dc97083ddba28b63bcb45bfda9cc7721620bc1
buidangduy05 <buidangduy243@gmail.com>
2026-05-01 11:29:09 +0700

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if (s[l]!=s[r]) return (isvalid(s.substr(0,l)+s.substr(l+1)) || isvalid(s.substr(0,r)+s.substr(r+1)));
+            l++; r--;
+        }
+        return true;
+    }
+    bool isvalid(string s){
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if (s[l]!=s[r]) return false;
+            l++; r--;
+        }
+        return true;
+    }
+};
